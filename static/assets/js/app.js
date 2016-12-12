84aba2b1ee05b26fce6086f0c092e7cefd122173
Rominou <befire@hotmail.fr>
2016-12-12 01:54:15 +0100

    First commit

---
+++ static/assets/js/app.js
@@ -0,0 +1,69 @@
+var API_URL = "http://api.romainmarbaix.dev/";
+
+/*
+ * Damn ie!
+ * https://teamtreehouse.com/community/background-attachment-is-messed-up-in-ie-and-microsoft-edge
+ */
+if(navigator.userAgent.match(/MSIE 10/i)
+  || navigator.userAgent.match(/Trident\/7\./)
+  || navigator.userAgent.match(/Edge/)) {
+    $('body').on("mousewheel", function () {
+        event.preventDefault();
+        var wd = event.wheelDelta;
+        var csp = window.pageYOffset;
+        window.scrollTo(0, csp - wd);
+    });
+}
+
+/*
+ * Contact form
+ */
+$( "#contact-form" ).submit(function(e){
+    e.preventDefault();
+
+    $.post(API_URL + "contact/send", $("#contact-form").serialize(), function (resp) {
+        resp = JSON.parse(resp);
+
+        if(resp.success) {
+            console.log('ok');
+        }
+        else {
+            console.log('notok');
+        }
+
+    });
+
+});
+
+var onLoad = function() {
+    /*
+     * Hide the loader
+     */
+    $('.loader').delay(200).fadeTo("slow", 0, function() {
+        $('.loader').hide();
+    });
+
+    /*
+     * Navbar is sticked to the top of the page when scrolling
+     */
+    $(window).scroll(function () {
+
+        var navbar = $('.navbar');
+
+        // Si le scroll est tout en haut de la page
+        if ($(window).scrollTop() > 35) {
+            if(! navbar.hasClass('unstick')) {
+                navbar.addClass('unstick');
+            }
+        }
+        // Si le scroll a dépassé le haut de la page
+        else {
+            if(navbar.hasClass('unstick')) {
+                navbar.removeClass('unstick');
+            }
+        }
+
+    });
+}();
+
+$( window ).load(onLoad);
